b77cf79fef6285a94a4426cdfbe5f99e73c6741e
youxin <534240118@qq.com>
2022-10-07 22:38:42 +0800

    策略工厂类优化加入单例模式

--- src/main/java/com/axin/design/pay/strategyFactory/StrategyFactory.java
+++ src/main/java/com/axin/design/pay/strategyFactory/StrategyFactory.java
@@ -3,15 +3,22 @@ package com.axin.design.pay.strategyFactory;
 import com.axin.design.pay.strategy.PayStrategy;
 import com.axin.design.pay.strategyEnum.StrategyEnum;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 // 工厂类依靠策略枚举返回策略类
 // 以后永远不需要修改factory,它是无状态的
 public class StrategyFactory {
+    private static final Map<String,PayStrategy> PAY_STRATEGY_MAP = new ConcurrentHashMap<>();
     public static PayStrategy getPayStrategy(StrategyEnum strategyEnum) {
-        PayStrategy payStrategy = null;
-        try {
-            payStrategy = (PayStrategy) Class.forName(strategyEnum.getValue()).newInstance();
-        }catch (Exception e){
-            // 异常
+        PayStrategy payStrategy = PAY_STRATEGY_MAP.get(strategyEnum.getValue());
+        if (payStrategy==null){
+            try {
+                payStrategy = (PayStrategy) Class.forName(strategyEnum.getValue()).newInstance();
+                PAY_STRATEGY_MAP.put(strategyEnum.getValue(),payStrategy);
+            }catch (Exception e){
+                // 异常
+            }
         }
         return payStrategy;
     }
